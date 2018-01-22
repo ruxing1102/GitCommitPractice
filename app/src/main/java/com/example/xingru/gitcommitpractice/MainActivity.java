5375035c972207c9cf89a46c5a4319daa4d25098
xing.ru <xing.ru@7fresh.com>
2018-01-22 13:23:47 +0800

    master 分支提交

--- app/src/main/java/com/example/xingru/gitcommitpractice/MainActivity.java
+++ app/src/main/java/com/example/xingru/gitcommitpractice/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //developer_one分支-第一次提交
         //master分支-第一次提交
     }
 }
